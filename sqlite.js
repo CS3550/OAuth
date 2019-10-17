9a0e282eb3bb38c567ba234f0690fd6679f12f6d
B Ricks <bricks@unomaha.edu>
2019-10-17 07:44:43 -0500

    Setup for day 10

--- sqlite.js
+++ sqlite.js
@@ -4,8 +4,9 @@ const path = require('path');
 const app = express()
 const jwt = require("./jwt_service")
 const flash = require("connect-flash");
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const sqlite3 = require('sqlite3')
 
 const secrets = require("./secrets.js")
 
@@ -17,8 +18,6 @@ app.use(session({ cookie: { maxAge: 60000 }, resave: false, saveUninitialized: f
 app.use(flash());
 
 app.use(express.static(path.join(__dirname, "public")));
-
-
 
 app.get("/login", (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${secrets.client_id}&redirect_uri=http://localhost:3000/callback&state=${secrets.state}`)
@@ -65,8 +64,21 @@ app.get("/callback", (req, res) => {
 })
 
 app.use((req, res, next) => {
-  console.log(req.path);
-  let bearer = req.flash('Bearer')[0];
+  //let bearer = req.flash('Bearer')[0];
+  let bearer;
+  console.log('Cookies: ', req.cookies)
+ 
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+  let flash = req.flash("Bearer");
+  if(flash && flash.length > 0){
+    bearer = flash[0];
+  }
+  else{
+    bearer = req.cookies["3550_Bearer"];
+    console.log("Couldn't find Bearer in flash")
+  }
+
   console.log(bearer)
 
   var verifyOptions = {
@@ -90,4 +102,24 @@ app.use((req, res, next) => {
 //If we get here, the user is authenticated.
 app.use(express.static(path.join(__dirname, "private")));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use("/query", (req, res)=>{
+  db.each("SELECT COUNT(logins) as count FROM LOGINS", (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(row.count );
+    res.send("" + row.count);
+    db.run("INSERT into LOGINS(logins) VALUES(?)", "0", (err)=>{
+      if(err){
+        return console.error("Error creating a new row:" + err);
+      }
+      console.log("Added a new row");
+    })
+  });
+})
+
+let db = new sqlite3.Database('./db/sqlite.db', (err) => {
+  if (err) return console.error("Error connecting to database: " + err.message + ". Terminating");
+  console.log('Connected to the database.');
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+});
